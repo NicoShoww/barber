8b710cf20be4e3d29456122a4153f51ff96ab58c
Nicholas Coelho <85971357+NicoShoww@users.noreply.github.com>
2023-11-22 21:16:46 -0300

    A

--- main.js
+++ main.js
@@ -31,19 +31,7 @@ function changeHeaderWhenScroll() {
 }
 
 /* Testimonials carousel slider swiper */
-const swiper = new Swiper('.swiper-container', {
-  slidesPerView: 1,
-  pagination: {
-    el: '.swiper-pagination'
-  },
-  keyboard: true,
-  breakpoints: {
-    767: {
-      slidesPerView: 2,
-      setWrapperSize: true
-    }
-  }
-})
+
 
 /* ScrollReveal: Mostrar elementos quando der scroll na página */
 const scrollReveal = ScrollReveal({

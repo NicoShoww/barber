b4ff355efc2ed07c3d5a5afbba3c06d8b69d29e3
Nicholas Coelho <85971357+NicoShoww@users.noreply.github.com>
2023-12-02 18:05:36 -0300

    a
    a

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ function changeHeaderWhenScroll() {
 /* Testimonials carousel slider swiper */
 if (document.querySelector('.swiper-container')) {
 const swiper = new Swiper('.swiper-container', {
-  slidesPerView: 1,
+  slidesPerView: 2,
   pagination: {
     el: '.swiper-pagination'
   },
@@ -41,7 +41,7 @@ const swiper = new Swiper('.swiper-container', {
   breakpoints: {
     767: {
       slidesPerView: 2,
-      setWrapperSize: true
+      setWrapperSize: false
     }
   }
 }); }
